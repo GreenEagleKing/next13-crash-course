624662ec6b5ac18a44d35eed56a70c34b2220be6
Ben King <benking1293@gmail.com>
2023-07-11 12:56:51 +0100

    Intial commit

---
+++ app/about/page.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About King Media</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem quis in
+        alias qui! Quidem inventore dolorum ex provident nemo unde reiciendis,
+        doloremque amet fugiat deserunt ab pariatur ipsa vero cumque eum quod
+        officiis autem possimus aliquid earum beatae, saepe consequuntur
+        eligendi voluptas. Atque dolorem soluta debitis blanditiis repellat
+        maxime dicta.
+      </p>
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ app/about/team/page.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+const TeamPage = () => {
+  return (
+    <div>
+      <h1>Team</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
+        praesentium cumque vitae fugit ipsam perspiciatis beatae quas
+        repellendus quidem explicabo impedit dicta sequi laboriosam ullam alias
+        ea, aperiam deleniti. Consequatur totam distinctio dolor nam suscipit,
+        ipsa voluptates dignissimos nulla saepe?
+      </p>
+    </div>
+  )
+}
+
+export default TeamPage
